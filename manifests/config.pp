887e3d53d4f8f65fbbaf3ec9de8cc3db6abdb516
Romain Tartière <romain@blogreen.org>
2016-06-15 17:24:54 +0200

    With v2.0, the default config vanished.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,9 @@
 class letsencrypt_sh::config {
-  file_line { "${letsencrypt_sh::etcdir}/config.sh-contact_email":
-    path  => "${letsencrypt_sh::etcdir}/config.sh",
-    line  => "CONTACT_EMAIL=${letsencrypt_sh::contact_email}",
-    match => '^#?CONTACT_EMAIL='
+  file { "${letsencrypt_sh::etcdir}/config":
+    ensure  => present,
+    owner   => $letsencrypt_sh::user,
+    group   => $letsencrypt_sh::user,
+    content => "CONTACT_EMAIL=${letsencrypt_sh::contact_email}\n",
   }
 
   file { "${letsencrypt_sh::etcdir}/.acme-challenges":
