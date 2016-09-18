738a19f4c862ed9d3705d7ee5270f239aa5e7671
Romain Tartière <romain@blogreen.org>
2016-09-18 11:25:29 +0200

    The config is now synced with upstream on FreeBSD

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,19 +1,18 @@
 class letsencrypt_sh::params {
   $user = 'letsencrypt'
+  $config = 'config'
 
   case $::osfamily {
     'Debian': {
       $apache_user = 'www-data'
       $etcdir = '/home/letsencrypt'
       $bin = '/home/letsencrypt/letsencrypt.sh'
-      $config = 'config'
     }
     'FreeBSD': {
       $apache_user = 'www'
       $bin = '/usr/local/bin/letsencrypt.sh'
       $package = 'letsencrypt.sh'
       $etcdir = '/usr/local/etc/letsencrypt.sh'
-      $config = 'config.sh'
     }
   }
 }
