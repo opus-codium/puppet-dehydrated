45a7a8c474993ca7aa755e6b515222a7a796e33a
Romain Tartière <romain@blogreen.org>
2016-10-20 13:40:30 +0200

    Apply apache configuration before requesting certificates

--- manifests/changed.pp
+++ manifests/changed.pp
@@ -6,4 +6,9 @@ class letsencrypt_sh::changed {
     path        => '/bin:/usr/bin:/usr/local/bin',
     user        => $letsencrypt_sh::user,
   }
+
+  if $letsencrypt_sh::apache_integration {
+    Class['Apache::Service'] ->
+    Exec["${letsencrypt_sh::bin} -c"]
+  }
 }
