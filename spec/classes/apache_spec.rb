a75b1c4e0996bbcf6f383a6a1da3ce1f1985190e
Romain Tartière <romain@blogreen.org>
2017-11-17 11:48:30 +0100

    Indent after last commit

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe 'dehydrated::apache' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
-  let(:facts) { facts }
+      let(:facts) { facts }
 
-  it { is_expected.to compile.with_all_deps }
+      it { is_expected.to compile.with_all_deps }
     end
   end
 end
